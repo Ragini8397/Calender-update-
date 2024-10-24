62775e8dec21b8b87981db80274fedd3fa3b3c28
Ragini8397 <raginikumari8397@gmail.com>
2024-10-24 13:07:33 +0530

    New changes are updated

--- calendar_api/events/models.py
+++ calendar_api/events/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+# events/models.py
 
-# Create your models here.
+from django.db import models
+
+class Event(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField(blank=True)
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
+    location = models.CharField(max_length=200, blank=True)
+    attendees = models.TextField(blank=True)  # You can customize this
+
+    def __str__(self):
+        return self.title
